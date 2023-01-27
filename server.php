cc365f2148b6414d667e9bb7ff5bfa95c3104c66
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2023-01-27 18:05:40 +0100

    first commit

---
+++ server.php
@@ -0,0 +1,13 @@
+<?php
+
+$jsonData = file_get_contents(__DIR__ . '/db/dischi.json');
+
+$decodedJson = json_decode($jsonData, true);
+
+if ($decodedJson != null) {
+    $discsList = $decodedJson;
+}
+
+
+
+echo $jsonData;
